ce7f4a6935894377da8f47039b86c7d3bed1a933
asus <princesstoy07@yahoo.com>
2023-07-21 16:25:30 +0000

    adding the requirment files

--- app.py
+++ app.py
@@ -1,62 +1,102 @@
-# Define the model path where the pre-trained model is saved on the Hugging Face model hub
-import gradio as gr
-import torch
+import pandas as pd
+import numpy as np
+import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from scipy.special import softmax
-import sympy
 
+# Define a function to transform the label values
+def transform_labels(label):
+    label = label['label']
+    num = 0
+    if label == -1: #'Negative'
+        num = 0
+    elif label == 0: #'Neutral'
+        num = 1
+    elif label == 1: #'Positive'
+        num = 2
+    return {'labels': num}
 
-# Define the model path where the pre-trained model is saved on the Hugging Face model hub
-model_path = "Preencez/finetuned-Sentiment-classfication-ROBERTA-model"
+# Define a dictionary to map integer class labels to their meanings
+class_meanings = {
+    0: "Negative",
+    1: "Neutral",
+    2: "Positive"
+}
 
-# Initialize the tokenizer for the pre-trained model (using RoBERTa tokenizer)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+# Example input label
+label = {'label': 0}
 
-# Load the pre-trained model
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
-# Define a function to preprocess the text data
-def preprocess(text):
-    new_text = []
-    # Replace user mentions with '@user'
-    for t in text.split(" "):
-        t = '@user' if t.startswith('@') and len(t) > 1 else t
-        # Replace links with 'http'
-        t = 'http' if t.startswith('http') else t
-        new_text.append(t)
-    # Join the preprocessed text
-    return " ".join(new_text)
+# Call the transform_labels function and get the transformed label
+transformed_label = transform_labels(label)
 
-# Define a function to perform sentiment analysis on the input text
-def sentiment_analysis(text):
-    # Preprocess the input text
-    text = preprocess(text)
+# Get the predicted class label and its meaning
+predicted_class = transformed_label['labels']
+predicted_class_meaning = class_meanings[predicted_class]
 
-    # Tokenize the input text using the pre-trained tokenizer
-    encoded_input = tokenizer(text, return_tensors='pt')
+# Example confidence level
+confidence_level = 1.16
 
-    # Feed the tokenized input to the pre-trained model and obtain output
-    output = model(**encoded_input)
+# Define the models dictionary
+models = {
+    "ROBERTA": "Preencez/finetuned-Sentiment-classfication-ROBERTA-model",
+}
 
-    # Obtain the prediction scores for the output
-    scores_ = output[0][0].detach().numpy()
+# Define the Streamlit app function
+def sentiment_analysis(model_name, text):
+    tokenizer = AutoTokenizer.from_pretrained(models[model_name])
+    model = AutoModelForSequenceClassification.from_pretrained(models[model_name])
+    
+    # Tokenize the input text
+    inputs = tokenizer(text, return_tensors="pt")
+    
+    # Make a forward pass through the model
+    outputs = model(**inputs)
+    
+    # Get the predicted class and associated score
+    predicted_class = outputs.logits.argmax().item()
+    score = outputs.logits.softmax(dim=1)[0][predicted_class].item()
 
-    # Apply softmax activation function to obtain probability distribution over the labels
-    scores_ = softmax(scores_)
+    # Compute the confidence level
+    confidence_level = np.max(outputs.logits.detach().numpy())
+    
+    # Display the predicted class and associated score
+    return f"Predicted class: {class_meanings[predicted_class]}, Score: {score:.3f}, Confidence Level: {confidence_level:.2f}"
 
-    # Format the output dictionary with the predicted scores
-    labels = ['Negative', 'Neutral', 'Positive']
-    scores = {l:float(s) for (l,s) in zip(labels, scores_) }
+# Define the main Streamlit app function
+def main():
+    st.title("Covid Tweets App")
+    st.image("https://www.aimlspectrum.com/wp-content/uploads/2022/01/ser-1-624x390.png")
 
-    # Return the scores
-    return scores
+    menu = ["About", "Home"]  # Switched the order of "About" and "Home"
+    choice = st.sidebar.selectbox("Menu", menu)
 
-# Define a Gradio interface to interact with the model
-demo = gr.Interface(
-    fn=sentiment_analysis, # Function to perform sentiment analysis
-    inputs=gr.Textbox(placeholder="Write your tweet here..."), # Text input field
-    outputs="label", # Output type (here, we only display the label with the highest score)
-    interpretation="default", # Interpretation mode
-    examples=[["Too God to be True!"]]) # Example input(s) to display on the interface
+    if choice == "About":
+        st.subheader("About")
+        st.write("This is a sentiment analysis NLP It uses a pre-trained model to predict the sentiment of the input text.")
 
-# Launch the Gradio interface
-demo.launch()
+    else:
+        st.subheader("Home")
+
+        # Add a dropdown menu to select the model
+        model_name = st.selectbox("Select a model", list(models.keys()))
+
+        with st.form(key="nlpForm"):
+            raw_text = st.text_area("Enter Text Here")
+            submit_button = st.form_submit_button(label="Analyze")
+
+        # Layout
+        col1, col2 = st.columns(2)
+        if submit_button:
+            # Display balloons
+            st.balloons()
+            with col1:
+                st.info("Results")
+                result = sentiment_analysis(model_name, raw_text)
+
+                # Print the predicted class and associated score
+                st.write(result)
+
+                # Display the predicted class meaning
+                st.write("Predicted Class Meaning:", class_meanings[predicted_class])
+
+if __name__ == "__main__":
+    main()
